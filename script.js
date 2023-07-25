20dc9994d9559b77eefac8bcb275ff64f9b0282e
ngo ba kha <anhemdaica123@gmail.com>
2023-07-25 21:03:43 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//Kiểm tra số chẵn  
+function check(){
+let a; 
+ a =document.getElementById("check");
+       
+              if (a % 2 == 0 ) {
+
+            alert( a + 'là số chẵn'); 
+        
+      } else { 
+            alert(a + 'là số lẻ'); }
+
+    }
+
